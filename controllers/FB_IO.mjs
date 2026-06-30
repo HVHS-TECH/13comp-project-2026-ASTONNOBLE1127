4ac2f053eb7b9daee004f69b8ac2bbf048ce831a
ASTONNOBLE1127 <23384an@hvhs.school.nz>
2026-07-01 11:56:29 +1200

    ../'s againnnnn

--- controllers/FB_IO.mjs
+++ controllers/FB_IO.mjs
@@ -307,7 +307,7 @@ export default class FB_IO {
                 if (await INSTANCES[FB_IO_INSTANCE].FB_Read(`/users/${_user.uid}/publicFixed/uid`) == _user.uid) {
                     //await console.log('success')
                     let deco = await INSTANCES[FB_IO_INSTANCE].FB_Read(`/users/${_user.uid}/public/profile decoration`)
-                    document.getElementById('deco').setAttribute('src',`../images/${deco}.png`)
+                    document.getElementById('deco').setAttribute('src',`./images/${deco}.png`)
                     INSTANCES[CONTENT_MANAGER_INSTANCE].changePage(Home_page)
                     document.getElementById('pfp').setAttribute('src',_user.photoURL)
                 }
